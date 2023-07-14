e592ea53c50170bdd289ecb6081cc1735e46739b
Coriana Williams <corianacory@gmail.com>
2023-07-13 23:39:07 -0400

    made updates to lab 40

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,24 +1,93 @@
 <template>
-  <main class="home d-flex flex-column align-items-center justify-content-center py-5 bg-light vh-100 text-dark">
-    <div class="logo-container d-flex justify-content-center align-items-center w-100">
-      <img class="logo" src="../assets/img/salmon.png" alt="Salmon logo">
-    </div>
-    
-    <h1 class="title fs-1 fw-bold text-dark text-center my-3">Salmon Cookies</h1>
-    
-    <div class="info fs-5 text-muted text-center lh-lg mx-auto" style="max-width: 800px;">
-      <p>Welcome to Salmon Cookies! A home-cooked, family-run business since 1989...</p>
+  <header>
+
+
+    <div class="wrapper">
+      <h1 class="title">Welcome to Pat's Salmon Cookies!</h1>
+    <img class="logo" src="../assets/img/salmon.png" alt="Salmon logo">
+  <main>
+    <div class="about">
+      <h1 class="about-title">About Us</h1>
+      <p class="about-description">We are a leading company in the industry, with a strong commitment to excellence and customer satisfaction. Our team is dedicated to providing the highest quality products and services.</p>
     </div>
   </main>
+
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/sales">Sales</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </nav>
+    </div>
+  </header>
+
+
+
+  <router-view />
 </template>
 
-<script>
-export default {
-  name: 'HomePage',
-}
-</script>
-
 <style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+
 .home {
   display: flex;
   flex-direction: column;
@@ -37,11 +106,6 @@ export default {
   width: 100%;
 }
 
-/* .logo {
-  width: 150px;
-  height: 150px;
-} */
-
 .title {
   font-size: 2.5em;
   font-weight: bold;
@@ -56,5 +120,30 @@ export default {
   line-height: 1.5;
   text-align: center;
   max-width: 800px;
+}
+
+.about {
+  margin-top: 2rem;
+  text-align: center;
+  padding: 0;
+
+}
+
+
+.about-title {
+  font-size: 1.8em;
+  font-weight: bold;
+  color: #333;
+  padding: 0;
+  margin-bottom: 5px;
+}
+
+.about-description {
+  font-size: 1em;
+  color: #666;
+  line-height: 1.4;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 0px;
 }
 </style>
--- src/views/SalesPage.vue
+++ src/views/SalesPage.vue
@@ -1,93 +1,250 @@
 <template>
-  <main class="py-4">
-    <h3 class="text-center mb-4">Welcome to Salmon Cookies' Internal Sales Data!</h3>
-
-    <!-- Include the AddStore component here -->
-    <AddStore @addStore="addStore"/>
-    
-    <!-- Now add the sales data table -->
-    <table class="table">
+  <div>
+<h3 class="sales-title">Pat's Sales Data</h3>
+<img class="logo" src="../assets/img/fish.jpg" alt="Salmon logo" style="width: 860px; height: 400px;">
+    <table class="sales-table">
       <thead>
         <tr>
-          <th>Location</th>
-          <th>Min Customers</th>
-          <th>Max Customers</th>
-          <th>Avg Cookies</th>
+          <th></th>
+          <th v-for="hour in hoursOfOperation" :key="hour">{{ hour }}</th>
+          <th>Daily Total</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(store, index) in stores" :key="index">
-          <td>{{ store.location }}</td>
-          <td>{{ store.min }}</td>
-          <td>{{ store.max }}</td>
-          <td>{{ store.avgCookies }}</td>
+        <tr v-for="location in storeLocation" :key="location.name">
+          <th>{{ location.name }}</th>
+          <td v-for="cookiesPurchased in location.cookiesPurchased" :key="cookiesPurchased">{{ cookiesPurchased }}</td>
+          <td>{{ location.dailyTotal }}</td>
         </tr>
       </tbody>
+      <tfoot>
+        <tr>
+          <td>Hourly Totals</td>
+          <td v-for="hourlyTotal in hourlyTotals" :key="hourlyTotal">{{ hourlyTotal }}</td>
+          <td>{{ grandTotal }}</td>
+        </tr>
+      </tfoot>
     </table>
-  </main>
+    <form @submit="handleSubmit">
+      <fieldset>
+        <legend>New Store Generator</legend>
+        <label for="storeLoc">Store Location:</label>
+        <input type="text" id="storeLoc" name="storeLoc">
+
+        <label for="minCap">Minimum Capacity:</label>
+        <input type="number" id="minCap" name="minCap">
+
+        <label for="maxCap">Maximum Capacity:</label>
+        <input type="number" id="maxCap" name="maxCap">
+
+        <label for="custAvgPurchase">Average Cookies Purchased per Customer:</label>
+        <input type="number" id="custAvgPurchase" name="custAvgPurchase">
+      </fieldset>
+      <button type="submit">Add Store Location</button>
+    </form>
+  </div>
 </template>
 
-
 <script>
-import StoreLocation from '../models/store-location.js';
-import AddStore from '../components/AddStore.vue';
-
 export default {
-  name: 'SalesPage',
-  components: {
-    AddStore,
-  },
   data() {
     return {
-      stores: [
-        new StoreLocation('Seattle', 23, 65, 6.3),
-        new StoreLocation('Tokyo', 3, 24, 1.2),
-        new StoreLocation('Dubai', 11, 38, 3.7)
-      ],
+      hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+      storeLocation: []
     };
   },
-  methods: {
-    addStore(storeData) {
-      const newStore = new StoreLocation(storeData.location, storeData.min, storeData.max, storeData.avgCookies);
-      this.stores.push(newStore);
+  computed: {
+    hourlyTotals() {
+      const totals = [];
+      for (let i = 0; i < this.hoursOfOperation.length; i++) {
+        let hourlyTotal = 0;
+        for (let j = 0; j < this.storeLocation.length; j++) {
+          hourlyTotal += this.storeLocation[j].cookiesPurchased[i];
+        }
+        totals.push(hourlyTotal);
+      }
+      return totals;
     },
+    grandTotal() {
+      let total = 0;
+      for (let j = 0; j < this.storeLocation.length; j++) {
+        total += this.storeLocation[j].dailyTotal;
+      }
+      return total;
+    }
   },
-}
+  methods: {
+    handleSubmit(event) {
+      event.preventDefault();
+      const storeLoc = event.target.storeLoc.value;
+      const minCap = +event.target.minCap.value;
+      const maxCap = +event.target.maxCap.value;
+      const custAvgPurchase = +event.target.custAvgPurchase.value;
+
+      const newStore = {
+        name: storeLoc,
+        minCust: minCap,
+        maxCust: maxCap,
+        avgCookiesPurchased: custAvgPurchase,
+        cookiesPurchased: [],
+        dailyTotal: 0
+      };
+
+      this.numOfCookies(newStore);
+      this.storeLocation.push(newStore);
+
+      event.target.reset();
+    },
+    numCust(store) {
+      return Math.floor(Math.random() * (store.maxCust - store.minCust + 1) + store.minCust);
+    },
+    numOfCookies(store) {
+      for (let i = 0; i < this.hoursOfOperation.length; i++) {
+        const cookieCount = Math.floor(this.numCust(store) * store.avgCookiesPurchased);
+        store.cookiesPurchased.push(cookieCount);
+        store.dailyTotal += cookieCount;
+      }
+    }
+  },
+  created() {
+    const seattle = {
+      name: 'Seattle',
+      minCust: 23,
+      maxCust: 65,
+      avgCookiesPurchased: 6.3,
+      cookiesPurchased: [],
+      dailyTotal: 0
+    };
+
+    const tokyo = {
+      name: 'Tokyo',
+      minCust: 3,
+      maxCust: 24,
+      avgCookiesPurchased: 1.2,
+      cookiesPurchased: [],
+      dailyTotal: 0
+    };
+
+    const dubai = {
+      name: 'Dubai',
+      minCust: 11,
+      maxCust: 38,
+      avgCookiesPurchased: 3.7,
+      cookiesPurchased: [],
+      dailyTotal: 0
+    };
+
+    const paris = {
+      name: 'Paris',
+      minCust: 20,
+      maxCust: 38,
+      avgCookiesPurchased: 2.3,
+      cookiesPurchased: [],
+      dailyTotal: 0
+    };
+
+    const lima = {
+      name: 'Lima',
+      minCust: 2,
+      maxCust: 16,
+      avgCookiesPurchased: 4.6,
+      cookiesPurchased: [],
+      dailyTotal: 0
+    };
+
+    this.numOfCookies(seattle);
+    this.numOfCookies(tokyo);
+    this.numOfCookies(dubai);
+    this.numOfCookies(paris);
+    this.numOfCookies(lima);
+
+    this.storeLocation.push(seattle, tokyo, dubai, paris, lima);
+  }
+};
 </script>
 
-
 <style scoped>
-main {
-  padding: 20px;
-}
-
-h3 {
+.sales-title {
   text-align: center;
   margin-bottom: 20px;
-}
-
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-table thead {
-  background-color: #f8f9fa;
-}
-
-table th, table td {
-  padding: 10px;
-  border: 1px solid #ccc;
-  text-align: left;
-}
-
-table th {
+  font-size: 55px;
+  color: #fff;
   font-weight: bold;
 }
 
-table tr:nth-child(even) {
-  background-color: #f2f2f2;
+.sales-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+  background-color: rgba(0, 0, 0, 0.2);
 }
+
+.sales-table thead {
+  background-color: rgba(0, 0, 0, 0.2);
+}
+
+.sales-table th,
+.sales-table td {
+  padding: 10px;
+  border: 1px solid #ccc;
+  text-align: left;
+  background-color: rgba(0, 0, 0, 0.2);
+  color: #fff;
+
+}
+
+.sales-table th {
+  background-color: rgba(0, 0, 0, 0.2);
+}
+
+.sales-table tr:nth-child(even) {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+.sales-table tfoot {
+  background-color: rgba(0, 0, 0, 0.2);
+}
+
+.sales-table tfoot td {
+  font-weight: bold;
+}
+
+.sales-table tr:hover {
+  background-color: rgba(0, 0, 0, 0.3);
+}
+
+.sales-table tr:before,
+.sales-table tr:after {
+  content: "";
+  display: table;
+}
+
+.sales-table tr:after {
+  clear: both;
+}
+
+.sales-table th,
+.sales-table td {
+  border-bottom: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+}
+
+.sales-table th:last-child,
+.sales-table td:last-child {
+  border-right: none;
+}
+
+.sales-table tbody tr:last-child td {
+  border-bottom: none;
+}
+
+.sales-table tbody tr:last-child th {
+  border-bottom: 1px solid #ccc;
+}
+
+.sales-table tbody tr:last-child th:last-child {
+  border-bottom: none;
+}
+
+
 </style>
-
-
